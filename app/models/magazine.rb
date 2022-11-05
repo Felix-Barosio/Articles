b584f57b329774b269c4b4848e76af021faad1c8
Felix <barosiofelix@gmail.com>
2022-11-05 17:36:08 +0300

    Added files via Upload

---
+++ app/models/magazine.rb
@@ -0,0 +1,58 @@
+class Magazine
+  attr_accessor :name, :category
+
+  @@all = []
+
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all << self
+
+  end
+
+
+  def articles
+
+    Article.all.filter {|article| article.magazine == self}
+
+  end
+
+
+  def article_titles
+
+    self.articles.map {|article| article.title}
+
+  end
+  
+
+  def contributors
+
+    self.articles.map {|article| article.author}.uniq
+
+  end
+
+
+  def contributing_authors
+    
+    magazine_authors = self.articles.map {|article| article.author}
+    authors =  magazine_authors.uniq
+    authors.filter {|author|  magazine_authors.count(author) > 2}
+
+  end 
+
+
+  def self.all
+
+    @@all
+
+  end
+
+
+  def self.find_by_name(name)
+
+    self.all.find {|magazine| magazine.name == name}
+
+  end
+
+
+end
